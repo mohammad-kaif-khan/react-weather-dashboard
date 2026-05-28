e042681f1f53acc3631677e3890bd5deb294f642
mohammad kaif khan <mohdkaifk456@gmail.com>
2026-05-28 11:56:34 +0530

    Cleaned up code and removed unused comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,4 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-// import React from "react";
-// import ReactDOM from "react-dom/client";
-// import App from "./App";
-// import "./index.css";
-
-// import { Provider } from "react-redux";
-// import { store } from "./app/store";
-// import { ThemeProvider } from "./context/ThemeContext";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <Provider store={store}>
-//     <ThemeProvider>
-//       <App />
-//     </ThemeProvider>
-//   </Provider>
-// );
 
 import React from "react";
 import ReactDOM from "react-dom/client";
